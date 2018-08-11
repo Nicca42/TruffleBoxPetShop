fad3fa1efc42afd6c65ca4b6d4b4184f62fa7df8
unknown <veronica.21.coutts@gmail.com>
2018-08-11 15:34:44 +0200

    finished adoption contract

--- pet-shop-tutorial/contracts/Adoption.sol
+++ pet-shop-tutorial/contracts/Adoption.sol
@@ -1,5 +1,19 @@
 pragma ^0.4.17;
 
 contract Adoption {
+    address[16] public adopters;
 
+    //addopting a pet
+    function adopt(uint petID) public returns (uint) {
+        require(petID >= 0 && petID <= 15);
+
+        adopters[petID] = msg.sender();
+
+        return petID;
+    }
+
+    //retrive the adopters
+    function getAdopters() public view returns(address[16]) {
+        return adopters;
+    }
 }
